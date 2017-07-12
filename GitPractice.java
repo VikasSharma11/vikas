d58633a756c3bfbb7b1519ced62a94bb3de1bfd2
VikasSharma11 <vsharma@salmon.com>
2017-07-13 00:36:12 +0530

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -2,10 +2,7 @@
 public class GitPractice {
 
 	public static void main(String[] args) {
-		System.err.println("this is first program from local");
-		System.err.println("This is second branch");
-		System.err.println("This is for repositry2222");
-	System.out.println("This is for vikas");
+	System.err.println("This is secondbrnach not copy at remote");
 	}
 
 }
