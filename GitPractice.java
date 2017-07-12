7add8c83894fe9266f1686bb8832eb900645fa54
VikasSharma11 <vsharma@salmon.com>
2017-07-13 00:10:35 +0530

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -9,8 +9,8 @@ public class GitPractice {
 	System.out.println("Now I am second branch at repositryMaster");
 	System.out.println("Now I am second branch at repositrysecond");
 	System.out.println("Now I am second branch at repositryatThird");
-	System.out.println("Edit at master local");
-	
+	System.out.println("Edit at master local from repositry");
+		System.out.println("Edit at master respostry from master branch");	
 	
 	}
 
