2cc43dc3148dff26be4459aa0a6e59f88ce3570f
VikasSharma11 <vsharma@salmon.com>
2017-07-13 09:48:03 +0530

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -2,21 +2,8 @@
 public class GitPractice {
 
 	public static void main(String[] args) {
-		System.err.println("this is first program from local");
-		System.err.println("This is second branch");
-		System.err.println("This is for repositry2222");
-	System.out.println("This is for vikas");
-	System.out.println("Now I am second branch at repositryMaster");
-	System.out.println("Now I am second branch at repositrysecond");
-	System.out.println("Now I am second branch at repositryatThird");
-	System.out.println("Edit at master local");
-	System.out.println("Edit at third branch");
-
-	System.out.println("Re-Edit at master local from local");
+		System.err.println("This is third branch");
 	
-	System.out.println("Edit at master local from repositry");
-		System.out.println("Edit at master respostry from master branch");	
-
 	}
 
 }
