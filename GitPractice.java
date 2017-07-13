947fb8d364acdb04eb5fc50531bf0f681d00b7d6
Vikas Sharma <vsharma@salmon.com>
2017-07-13 10:27:41 +0530

    te

--- GitPractice.java
+++ GitPractice.java
@@ -2,7 +2,7 @@
 public class GitPractice {
 
 	public static void main(String[] args) {
-	System.err.println("This is secondbrnach not copy at remote");
+	System.err.println("This is secondbrnach lewand not copy at remote");
 	}
 
 }
