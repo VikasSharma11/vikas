5d9a2c6eab28f2cfd8958df4abd521017dc96069
VikasSharma11 <vsharma@salmon.com>
2017-07-13 09:46:53 +0530

    Update GitPractice.java

--- GitPractice.java
+++ GitPractice.java
@@ -3,9 +3,7 @@ public class GitPractice {
 
 	public static void main(String[] args) {
 
-		System.err.println("This is masterbranch from Local");
-
-		System.err.println("This is masterbranch at remote");
+	System.err.println("This is masterbranch at remote");
 		
 }
 
